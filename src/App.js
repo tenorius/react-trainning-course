a568392bc4b15dc77d6ff127928616ce91be7483
tenorio <rodrigo.o.tenorio@accenture.com>
2019-04-24 19:34:26 -0300

    aula pratica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    form: {
+      id: ''
+    }
+  };
+  
+  handleChange = (e) => {
+    const { value } = e.target;
+  }
+  
   render() {
+    
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton  color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" >
+              Pokedex
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+        <Paper className="content">
+          <form  noValidate autoComplete="off">
+            <div className="form-control">
+              <TextField
+                id="standard-name"
+                label="Name"
+                value={this.state.form.id}
+                onChange={this.handleChange}
+                margin="normal"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className="button"
+              >
+                Search
+              </Button>
+            </div>
+          </form>
+          
+          
+          <section className="pokemon-container">
+  
+            <Card className="card">
+              <CardActionArea>
+                <CardMedia
+                  className="media"
+                  image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                  </Typography>
+                  <Typography component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </section>
+        </Paper>
+        
+        
+        
+        </main>
       </div>
     );
   }
